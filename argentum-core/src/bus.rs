fd125a70de7b39f76ff6bcac7d981dbc1a323dc1
Anish Jewalikar <anishjewalikar@gmail.com>
2021-01-16 16:16:20 +0530

    [core] implement an ad hoc bus interface.

---
+++ argentum-core/src/bus.rs
@@ -0,0 +1,40 @@
+//! Contains implementation of the Game Boy memory bus interface.
+
+/// The Game Boy memory bus.
+pub struct Bus {
+    // Ad hoc implementation.
+    memory: Box<[u8; u16::MAX as usize + 1]>,
+}
+
+impl Bus {
+    /// Create a new `Bus` instance.
+    pub fn new(rom_buffer: &[u8]) -> Self {
+        let mut memory = Box::new([0; u16::MAX as usize + 1]);
+
+        // Load the ROM into memory.
+        memory[..rom_buffer.len()].copy_from_slice(rom_buffer);
+
+        Self { memory }
+    }
+
+    /// Read a byte from the specified address.
+    pub fn read_byte(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    /// Write a byte to the specified address.
+    pub fn write_byte(&mut self, addr: u16, value: u8) {
+        self.memory[addr as usize] = value;
+    }
+
+    /// Read a word from the specified address.
+    pub fn read_word(&self, addr: u16) -> u16 {
+        (self.read_byte(addr) as u16) | ((self.read_byte(addr + 1) as u16) << 8)
+    }
+
+    /// Write a word to the specified address.
+    pub fn write_word(&mut self, addr: u16, value: u16) {
+        self.write_byte(addr, value as u8);
+        self.write_byte(addr + 1, (value >> 8) as u8);
+    }
+}
--- argentum-core/src/cpu/mod.rs
+++ argentum-core/src/cpu/mod.rs
@@ -3,7 +3,8 @@
 
 mod registers;
 
-use registers::*;
+use self::registers::*;
+use crate::bus::Bus;
 
 /// Implementation of the Sharp SM83.
 pub struct Cpu {
@@ -25,5 +26,21 @@ impl Cpu {
             ime: false,
             pc: 0x0000,
         }
+    }
+
+    /// Read a byte from the address pointed to by `PC`.
+    pub fn imm_byte(&mut self, bus: &mut Bus) -> u8 {
+        let value = bus.read_byte(self.pc);
+        self.pc += 1;
+
+        value
+    }
+
+    /// Read a word from the address pointed to by `PC`.
+    pub fn imm_word(&mut self, bus: &mut Bus) -> u16 {
+        let value = bus.read_word(self.pc);
+        self.pc += 2;
+
+        value
     }
 }
