64b3abba124b620535f243f12f9edeeb166b8609
Anish Jewalikar <anishjewalikar@gmail.com>
2021-10-01 19:01:38 +0530

    Core: Add explicit length to bootrom statics.

--- crates/argentum/src/bus/bootrom.rs
+++ crates/argentum/src/bus/bootrom.rs
@@ -1,5 +1,5 @@
 /// SameBoot for the DMG, made by LIJI.
-pub static DMG_BOOT_ROM: &[u8] = include_bytes!("bootrom/dmg_boot.bin");
+pub static DMG_BOOT_ROM: &[u8; 0x100] = include_bytes!("bootrom/dmg_boot.bin");
 
 /// SameBoot for the CGB, made by LIJI.
-pub static CGB_BOOT_ROM: &[u8] = include_bytes!("bootrom/cgb_boot.bin");
+pub static CGB_BOOT_ROM: &[u8; 0x900] = include_bytes!("bootrom/cgb_boot.bin");
